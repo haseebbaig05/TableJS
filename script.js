89975dd6a2f81b8879625bcc307a432a917399fc
haseebbaig05 <154249225+haseebbaig05@users.noreply.github.com>
2024-01-26 11:26:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const y = prompt("Enter the number of the table...")
+for (let a = y; a <= 20; a++){
+    for (let m = 1; m<=10; m++){
+        result =m *a
+    document.write(`${a} x ${m} =${result} <br>`)
+
+    }
+    document.write("<br>")
+}
+
